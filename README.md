6e7f2810263753a2da93a27a7ba2b39dffd29ec6
making <makingx@gmail.com>
2017-06-28 14:56:33 +0900

    Add service registry

--- README.md
+++ README.md
@@ -8,7 +8,23 @@ Just run
 cf push --random-route
 ```
 
-You can access `https://demo-payara-micro-<random-words>.cfapps.io`.
+You can access `https://tiny-service-registry-<random-words>.cfapps.io`.
 
 
-If you want to use a database, **check [db branch](https://github.com/making/cf-payara-micro/tree/db)**.
+### Register a service instance
+
+```
+curl -XPOST -H "Content-Length: 0" "https://tiny-service-registry-<random-words>.cfapps.io/api/services/foo?host=xxxx&port=xxx"
+```
+
+### Delete a service instance
+
+```
+curl -XDELETE "https://tiny-service-registry-<random-words>.cfapps.io/api/services/foo?host=xxxx&port=xxx"
+```
+
+### Get all service instances
+
+```
+curl https://tiny-service-registry-<random-words>.cfapps.io/api/services/foo
+```
---
+++ src/main/java/com/example/SimpleRestApi.java
@@ -0,0 +1,127 @@
+
+/*
+ * Copyright (c) 2008-2016, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import javax.inject.Singleton;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Singleton
+@Path("api/services")
+public class SimpleRestApi {
+
+	private final ConcurrentMap<String, Set<Address>> addressScopes = new ConcurrentHashMap<>();
+
+	@POST
+	@Path("{scope}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response register(@PathParam("scope") String scope,
+			@QueryParam("host") String host, @QueryParam("port") int port) {
+		Set<Address> addresses = getAddresses(scope);
+		if (addresses.add(new Address(host, port))) {
+			return Response.ok()
+					.entity("{ \"host\": \"" + host + "\", \"port\": " + port + " }")
+					.build();
+		}
+		return Response.notModified().build();
+	}
+
+	@DELETE
+	@Path("{scope}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response delete(@PathParam("scope") String scope,
+			@QueryParam("host") String host, @QueryParam("port") int port) {
+		Set<Address> addresses = getAddresses(scope);
+		if (addresses.remove(new Address(host, port))) {
+			return Response.ok().build();
+		}
+		return Response.notModified().build();
+	}
+
+	@GET
+	@Path("{scope}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response services(@PathParam("scope") String scope) {
+		Set<Address> addresses = getAddresses(scope);
+		StringBuilder sb = new StringBuilder(
+				"{ \"service\": \"" + scope + "\", \"endpoints\": [");
+		for (Address address : addresses) {
+			sb.append("{ \"host\": \"" + address.host + "\", \"port\": " + address.port
+					+ " },");
+		}
+		sb.deleteCharAt(sb.length() - 1).append("] }");
+		return Response.ok().entity(sb.toString()).build();
+	}
+
+	private Set<Address> getAddresses(String scope) {
+		Set<Address> addresses = addressScopes.get(scope);
+		if (addresses == null) {
+			addresses = newThreadSafeSet();
+			Set<Address> temp = addressScopes.putIfAbsent(scope, addresses);
+			if (temp != null) {
+				addresses = temp;
+			}
+		}
+		return addresses;
+	}
+
+	private Set<Address> newThreadSafeSet() {
+		return Collections.newSetFromMap(new ConcurrentHashMap<Address, Boolean>());
+	}
+
+	private static class Address {
+		private final String host;
+		private final int port;
+
+		private Address(String host, int port) {
+			this.host = host;
+			this.port = port;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) {
+				return true;
+			}
+			if (o == null || getClass() != o.getClass()) {
+				return false;
+			}
+
+			Address address = (Address) o;
+
+			if (port != address.port) {
+				return false;
+			}
+			return host != null ? host.equals(address.host) : address.host == null;
+		}
+
+		@Override
+		public int hashCode() {
+			int result = host != null ? host.hashCode() : 0;
+			result = 31 * result + port;
+			return result;
+		}
+	}
+
+}
